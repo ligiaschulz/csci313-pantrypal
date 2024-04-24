7bb08d4c5ce133ceea3a000f54a4f9bbdd2f5496
CaitlinAzazel <caitlin.frank03@gmail.com>
2024-04-24 15:01:03 -0500

    added files to recipe
    added urls.py, views.py, and index.html

--- pantrypal/recipe/views.py
+++ pantrypal/recipe/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    user_list = User.objects.all()
+    return render(request, 'recipe/index.html')
+
