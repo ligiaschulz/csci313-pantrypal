536394529f900db90b5e8f8348c70362a6cd2b3d
CaitlinAzazel <caitlin.frank03@gmail.com>
2024-04-24 15:02:32 -0500

    saved files forgot to add
    added versions of recipe.html and urls.py

--- pantrypal/recipe/urls.py
+++ pantrypal/recipe/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base'),
-    path('recipe/', views.recipe, name='accounts'),
+    path('', views.index, name='index'),
+    path('accounts/<int:pk>/', views.account_view, name='accounts'),
 ]
