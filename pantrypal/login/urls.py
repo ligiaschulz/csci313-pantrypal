b8d8f7c2517f5b019d1a7d04661db71ad31b2742
Kaitlyn Nickel <nickelk102703@gmail.com>
2024-04-24 15:11:25 -0500

    Created logout and added to navbar

--- pantrypal/login/urls.py
+++ pantrypal/login/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns= [
 path('login/',views.login_user,name = 'login'),
+path('logout/', views.logout_user, name = 'logout'),
 ]
--- pantrypal/login/views.py
+++ pantrypal/login/views.py
@@ -17,3 +17,8 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, 'login/login.html', {})
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out successfully')
+    return redirect ('index')
