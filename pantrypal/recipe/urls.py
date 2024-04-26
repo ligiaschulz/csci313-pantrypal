4ecc679a83afa07d0a39430656dd9ba2eaa5a0f9
Ligia Schulz <schulzligia@gmail.com>
2024-04-26 14:05:31 -0500

    Recipe urls and navbar browse link
    Recipe urls are included! woohoo

--- pantrypal/recipe/urls.py
+++ pantrypal/recipe/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('accounts/', views.account_view, name='accounts'),
-    path('delete/<str:pk>', views.delete, name="delete"),
+    
 ]
