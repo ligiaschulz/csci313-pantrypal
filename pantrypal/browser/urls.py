4cad87b4aeae1aaf08deb875d7f6c28633c181ce
Ligia Schulz <schulzligia@gmail.com>
2024-04-26 13:12:17 -0500

    Implemented category filter
    Added functionality to filter recipes by category

--- pantrypal/browser/urls.py
+++ pantrypal/browser/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.RecipeListView.as_view(), name='browse-all'),
-    path('search/', views.filter)
+    path('', views.browse_all, name='browse-all'),
+    path('category<int:pk>/', views.category_filter, name='category-filter'),
 ]
--- pantrypal/browser/views.py
+++ pantrypal/browser/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
-from recipe.models import Recipe
+from recipe.models import Recipe, Category
 
-
-class RecipeListView(generic.ListView):
-    model = Recipe
-    template_name='browser/browse.html'
-
-def filter(request):
+def browse_all(request):
     recipe_list = Recipe.objects.all()
-    context = {'recipe_list':recipe_list}
+    category_list = Category.objects.all()
+    context = {'recipe_list':recipe_list, 'category_list':category_list}
+    return render(request, 'browser/browse.html', context=context)
+
+def category_filter(request, pk):
+    selected_category=Category.objects.get(pk=pk)
+    recipe_list = Recipe.objects.filter(category=selected_category)
+    category_list = Category.objects.all()
+    context = {'recipe_list':recipe_list, 'category_list':category_list, 'selected_category':selected_category}
     return render(request, 'browser/browse.html', context=context)
