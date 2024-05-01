8a04ff586e2fc1af7bdf85f2ba70ac6c710628ad
Kaitlyn Nickel <nickelk102703@gmail.com>
2024-05-01 15:04:52 -0500

    Added the update user functionality

--- pantrypal/login/forms.py
+++ pantrypal/login/forms.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -35,3 +35,28 @@ class SignUpForm(UserCreationForm):
         self.fields["password2"].widget.attrs["class"] = "form-control"
         self.fields["password2"].widget.attrs['placeholder'] = "Password"
         self.fields["password2"].label = ""
+
+class UpdateUserForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email','username']
+    
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs["class"] = "form-control"
+        self.fields["username"].widget.attrs['placeholder'] = "User Name"
+        self.fields["username"].label = ""
+
+        self.fields["first_name"].widget.attrs["class"] = "form-control"
+        self.fields["first_name"].widget.attrs['placeholder'] = "First Name"
+        self.fields["first_name"].label = ""
+
+        self.fields["last_name"].widget.attrs["class"] = "form-control"
+        self.fields["last_name"].widget.attrs['placeholder'] = "Last Name"
+        self.fields["last_name"].label = ""
+
+        self.fields["email"].widget.attrs["class"] = "form-control"
+        self.fields["email"].widget.attrs['placeholder'] = "Email Address"
+        self.fields["email"].label = ""
