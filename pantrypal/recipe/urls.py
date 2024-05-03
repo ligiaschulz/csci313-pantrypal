96b54226e7fe395ab4ee8d4edd44e350a08b7c31
Ligia Schulz <schulzligia@gmail.com>
2024-05-03 10:43:57 -0500

    Fixed recipe urls
    They were messed up

--- pantrypal/recipe/urls.py
+++ pantrypal/recipe/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.recipe_detail, name='recipe_detail'),
     path('create/', views.RecipeAddView.as_view(), name='recipe-create'),
-    path('create/ingredient/<int:pk>', views.create_recipeline, name='recipe-ingredients'),
     path('<int:recipe_id>/', views.recipe_detail, name='recipe-ingredients'),
 ]
